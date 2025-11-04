2c5c912d4f937527cd16a50daaa08be75bc7cb69
coded-anshul <anshuljain2106@gmail.com>
2025-11-04 11:52:53 +0530

    greatest number

---
+++ day01/day01.js
@@ -0,0 +1,32 @@
+// var p = Number(prompt("Enter Principle"));
+// var r = Number(prompt("Enter Rate"));
+// var t = Number(prompt("Enter Time"));
+
+// var a = p* Math.pow(1 + r/100, t);
+
+// var CI = a - p;
+// console.log(CI);
+
+//herons formula
+//s=a+b+c/2
+//sqrt(s*(s-s)*(s-b)*(s-c))
+
+// var a = Number(prompt("enter side a"));
+// var b = Number(prompt ("enter side b"));
+// var c= Number(prompt("enter side c"));
+// 
+// if (a+b>c || a+c>b||b+c>a){
+    // var s=(a+b+c)/2;
+    // console.log(Math.sqrt(s*(s-a)*(s-b)*(s-c)));
+// } 
+// else{
+    // console.log("this is not a right angle traingle");
+// }
+
+
+//var radius = Number(prompt("enter the radius"));
+//var pie= 3.14;
+//var area = 2*pie*radius;
+//console.log(area);
+
+
---
+++ day03/day03.js
@@ -0,0 +1,14 @@
+var a = Number(prompt("enter the first number"));
+var b = Number(prompt("enter the second number"));
+
+if (a>b){
+    console.log(a);
+} 
+if (b>a){
+    console.log(b);
+}
+if (a==b){
+    console.log("equal");
+}
+
+
