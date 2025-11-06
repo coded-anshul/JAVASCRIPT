b20f57f04b5e94b8f6ab8303d40370dff3d892ef
coded-anshul <anshuljain2106@gmail.com>
2025-11-06 14:19:20 +0530

    qna

--- day04/day04.js
+++ day04/day04.js
@@ -62,3 +62,4 @@
 //    var isPrime = false;
 //    break;
 //}
+
---
+++ day05/day05.js
@@ -0,0 +1,39 @@
+//break and continue 
+
+//for(i=0;i<=10;i++){
+//    if (i==5) break
+//    console.log(i);
+//}
+//
+//for(i=0;i<=10;i++){
+//    if (i==5) continue
+//    console.log(i);
+//}
+
+
+//while loop
+
+// while (true){
+//     code
+// }
+
+
+//var num = Number(prompt("enter the num"));
+//
+//var a = num % 10;
+//var b = Math.floor(num/10);
+//var c = a * 10
+//
+//console.log (c + b);
+//
+
+
+//var num = Number(prompt("enter the num"));
+//var square= (num*num);
+//
+//if(square%10==num){
+//    console.log("automorphic");
+//}
+//else{
+//    console.log("not automorphic")
+//}
