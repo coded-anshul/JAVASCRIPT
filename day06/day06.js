3f9cb8091058f5f2546758d7e24cfc3a131ecc39
coded-anshul <anshuljain2106@gmail.com>
2025-11-07 14:55:04 +0530

    calculator

--- day06/day06.js
+++ day06/day06.js
@@ -15,21 +15,43 @@
 //}while(user=="yes");
 
 //guess the number game
-var num = Math.floor(Math.random()*100+1);
-var usernum=Math.floor(Number(prompt("enter the number")));
-do{
-    if (usernum<0 && usernum >100 || usernum ==NaN){
-        var usernum=Math.floor(Number(prompt("enter a valid num b/w 1 to 100")));
+//var num = Math.floor(Math.random()*100+1);
+//var usernum=Math.floor(Number(prompt("enter the number")));
+//do{
+//    if (usernum<0 && usernum >100 || usernum ==NaN){
+//        var usernum=Math.floor(Number(prompt("enter a valid num b/w 1 to 100")));
+//
+//    }
+//    else if (usernum>num){
+//        var usernum=Math.floor(Number(prompt("too high , try again")));
+//
+//    }
+//    else if (usernum<num){
+//        var usernum=Math.floor(Number(prompt("too low, try again")));
+//    }
+//    
+//}while(usernum!==num)
+//
+//console.log("correct answer");
 
-    }
-    else if (usernum>num){
-        var usernum=Math.floor(Number(prompt("too high , try again")));
+//sasta calculator
 
-    }
-    else if (usernum<num){
-        var usernum=Math.floor(Number(prompt("too low, try again")));
-    }
-    
-}while(usernum!==num)
 
-console.log("correct answer");
+//do{
+//    var num1= Number(prompt("enter the first number"));
+//    var num2= Number(prompt("enter the second number"));
+//    var operator=prompt("enter +,-,*,/");
+//    
+//    switch(operator){
+//        case "+" : console.log(num1 + num2);
+//        break
+//        case "-" : console.log(num1-num2);
+//        break
+//        case "*" : console.log(num1 * num2) ;
+//        break
+//        case "/" : console.log(num1/num2);
+//        break
+//        default: console.log("bhund mai nimbu nichod ke pija");
+//    }
+//    var user=prompt("do you want to continue yes/no").toLowerCase();
+//}while(user=="yes");
