42b8ac035201c8fddfdb4f20d46b7feb2dad3ef1
coded-anshul <anshuljain2106@gmail.com>
2025-11-07 14:37:42 +0530

    num b/w 1 to 100

--- day06/day06.js
+++ day06/day06.js
@@ -5,6 +5,8 @@
 //    i++;
 //}while (i<=5);
 
+//const { use } = require("react");
+
 //ek na ek bar chalega loop 
 
 //do{
@@ -12,3 +14,22 @@
 //    var user = prompt("yes or no").toLowerCase;
 //}while(user=="yes");
 
+//guess the number game
+var num = Math.floor(Math.random()*100+1);
+var usernum=Math.floor(Number(prompt("enter the number")));
+do{
+    if (usernum<0 && usernum >100 || usernum ==NaN){
+        var usernum=Math.floor(Number(prompt("enter a valid num b/w 1 to 100")));
+
+    }
+    else if (usernum>num){
+        var usernum=Math.floor(Number(prompt("too high , try again")));
+
+    }
+    else if (usernum<num){
+        var usernum=Math.floor(Number(prompt("too low, try again")));
+    }
+    
+}while(usernum!==num)
+
+console.log("correct answer");
