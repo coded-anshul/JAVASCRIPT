61df0f1fbf8305da84883af9a7ebf6323553a191
coded-anshul <anshuljain2106@gmail.com>
2025-11-10 15:29:35 +0530

    pattern programming

---
+++ day07/day07.js
@@ -0,0 +1,79 @@
+//ascii code
+
+//for (i=0;i<=40000;i++){
+//    process.stdout.write(String.fromCharCode(i));
+//}
+
+
+//let temp = 65;
+//for (let i = 65; i <= 69; i++) {
+//  for (let j = 65; j <= i; j++) {
+//    process.stdout.write(String.fromCharCode(j) + ' ');
+//  }
+//
+//  console.log(" ");}
+
+
+/*
+        *
+      * * 
+    * * * 
+  * * * *
+* * * * * 
+
+*/
+
+
+//for (let i = 1; i <= 5; i++) {
+//  for (let j = 1; j <= 5 - i; j++) {
+//    process.stdout.write("  ");
+//  }
+//
+//  for (let j = 1; j <= i; j++) {
+//    process.stdout.write("* ");
+//  }
+//  process.stdout.write("\n");
+//}
+//
+
+//for (let i = 1; i <= 5; i++) {
+//  for (let j = 1; j <= 5 - i; j++) {
+//    process.stdout.write(" ");
+//  }
+//
+//  for (let j = 1; j <= i; j++) {
+//    process.stdout.write("* ");
+//  }
+//  process.stdout.write("\n");
+//}
+
+
+//for (let i = 0; i <= 5; i++) {
+//  for (let s = 0; s <i; s++) {
+//    process.stdout.write("  ");
+//  }
+//
+//  for (let j = i; j < 5; j++) {
+//    process.stdout.write("* ");
+//  }
+//  process.stdout.write("\n");
+//}
+
+
+
+
+
+//for (let i = 1; i <= 5; i++) {
+//  for (let j = 1; j <= 5 - i; j++) {
+//    process.stdout.write(" ");
+//  }
+//
+//  for (let j = 1; j <= i; j++) {
+//    process.stdout.write("* ");
+//  }
+//  process.stdout.write("\n");
+//}
+
+
+
+
