e3d691f837f90eeefb7fd380e5ef4acc05010ef4
coded-anshul <anshuljain2106@gmail.com>
2025-11-14 11:03:01 +0530

    GCD/HCF

---
+++ Practice/practice.js
@@ -0,0 +1,15 @@
+//let a = parseInt(prompt("Enter first number (a): "));
+//let b = parseInt(prompt("Enter second number (b): "));
+//
+//function gcd(a, b) {
+//    while (b !== 0) {
+//        let temp = b;
+//        b = a % b;
+//        a = temp;
+//    }
+//    return a;
+//}
+//
+//let result = gcd(a, b);
+//console.log("GCD / HCF is: " + result);
+//alert("GCD / HCF is: " + result);
